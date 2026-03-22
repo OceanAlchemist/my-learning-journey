b5684aad8cdc60f2fca29e81e3f12743efa58dde
OceanAlchemist <2189552305@qq.com>
2026-03-22 21:26:41 +0800

    160题：利用双指针和三目运算符

---
+++ 160_intersection_of_two_linked_lists.c
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if(!headA||!headB)return NULL;
+   struct ListNode*a=headA;struct ListNode*b=headB;
+    while(a!=b)
+    {a=a?a->next:headB;
+    b=b?b->next:headA;}
+    return a;
+}
