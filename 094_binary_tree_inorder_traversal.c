c9d6d815fcc04dd4667328fbeaf1ecb0b1dde43f
OceanAlchemist <2189552305@qq.com>
2026-05-13 21:46:07 +0800

    94题：二叉树的中序遍历，递归+函数+数组

---
+++ 094_binary_tree_inorder_traversal.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void inorder(struct TreeNode* root, int* res, int* returnSize) {
+    if (root == NULL) return;
+    inorder(root->left, res, returnSize);
+    res[(*returnSize)++] = root->val;
+    inorder(root->right, res, returnSize);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* res = (int*)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    inorder(root, res, returnSize);
+    return res;
+}
